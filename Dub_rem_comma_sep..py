7b220844f8a3817af917ef31187765ef73927882
Himadrib <33131731+Himadrib@users.noreply.github.com>
2017-10-27 01:00:33 +0530

    Update Dub_rem_comma_sep..py

--- Dub_rem_comma_sep..py
+++ Dub_rem_comma_sep..py
@@ -1,7 +1,8 @@
 import time
 lines_st = set() #lines stored 
-outputfile = open("output.txt", "w")
-for line in open("text.txt", "r"):
+outputfile = open("output.txt", "w") #output file  and file name
+filename=input("Input the face name: ") #take your file name as input
+for line in open(filename, "r"):
     if line not in lines_st: # Unique line check
         outputfile.write(line.replace('\n',", ")) # replace new line with comma
         lines_st.add(line)
